5b1447f03b7fca0798f0019a573c47ccb0e23e88
liuqi <liuqi@hgobox.com>
2018-12-21 16:41:47 +0800

    ini

--- test.py
+++ test.py
@@ -1,27 +1,26 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import qrcode
 import json
 import os
 import shutil
-# img = qrcode.make('Some data here')
-# img.show()
-
 
 def generate(data):
     for vm_code, url in data.items():
-        im = Image.open('temp.jpg')
-        print('im info keys', im.info.keys())
-        print('dpi', im.info['dpi'])
-
+        im = Image.open('meituan.jpg')
         exif = im.info['exif']
-        qr_img = qrcode.make(url, box_size=22)
+        qr_img = qrcode.make(url, box_size=22, border =1 )
         (w, h) = qr_img.size
-        x = int((750 - w) / 2 + 290)  # 居中
-        y = 250
+        x = int((730 - w) / 2 + 226)  # 居中
+        y = int((730 - w) / 2 + 406)
         box = (x, y, x + w, y + h)
         im.paste(qr_img, box)
-        im.save('./result/{}.jpg'.format(vm_code),
-                'jpeg', exif=exif, quanlity=100)
+
+        # 文字
+        draw = ImageDraw.Draw(im)
+        font = ImageFont.truetype("arial.ttf", 40)
+        draw.text((662, 3702), 'SN:' + vm_code, font=font, fill=(0, 0, 0, 0))
+        # im.show()
+        im.save('./result/{}.jpg'.format(vm_code),'jpeg', exif=exif, quanlity=100)
 
 
 with open('data.json') as f:
